01a8287154cd81eef02e0241073db895ce8a9fb3
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-02-21 13:47:08 -0400

    pequenos detalhes

--- latex_image.py
+++ latex_image.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import sys
 
+# noremap <leader>a yiB :execute '! python ~/Documentos/devel/tools/latex_image.py ' . shellescape(@",1) <CR>
 with Image.open(sys.argv[1]) as im:
     width, height = im.size
-    print(width/height)
+    print(f'{width/height:.2f}')
