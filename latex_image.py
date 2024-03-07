47c1fda094332eb8eb8bd8d44945d55515ba0464
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-03-07 18:32:15 -0400

    pequenos consertos

--- latex_image.py
+++ latex_image.py
@@ -1,7 +1,12 @@
+from pathlib import Path
 from PIL import Image
 import sys
 
 # noremap <leader>a yiB :execute '! python ~/Documentos/devel/tools/latex_image.py ' . shellescape(@",1) <CR>
-with Image.open(sys.argv[1]) as im:
+q = Path(sys.argv[1])
+if not q.exists():
+    q=list(Path().glob(sys.argv[1]+"*"))[0]
+
+with Image.open(q) as im:
     width, height = im.size
     print(f'{width/height:.2f}')
