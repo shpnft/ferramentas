e9e99568baa3cdd86003659c2e3c316af760dda4
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-03-07 18:42:08 -0400

    pequenos consertos

--- latex_image.py
+++ latex_image.py
@@ -5,7 +5,7 @@ import sys
 # noremap <leader>a yiB :execute '! python ~/Documentos/devel/tools/latex_image.py ' . shellescape(@",1) <CR>
 q = Path(sys.argv[1])
 if not q.exists():
-    q=list(Path().glob(sys.argv[1]+"*"))[0]
+    q=next(Path().glob(sys.argv[1]+"*"))
 
 with Image.open(q) as im:
     width, height = im.size
