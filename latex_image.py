eebbb195a14c7f3c753989e18b28251d49fe71ef
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-03-10 13:30:21 -0400

    pequenos consertos

--- latex_image.py
+++ latex_image.py
@@ -2,9 +2,10 @@ from pathlib import Path
 from PIL import Image
 import sys
 
-# noremap <leader>a yiB :execute '! python ~/Documentos/devel/tools/latex_image.py ' . shellescape(@",1) <CR>
+#noremap <leader>a mcyiB :let @a=system("python ~/Documentos/devel/tools/latex_image.py " . shellescape(@",1)) <CR>`c"ap
+
 q=next(Path().glob(sys.argv[1]+"*"))
 
 with Image.open(q) as im:
     width, height = im.size
-    print(f'{width/height:.2f}')
+    print(f'\\real{{{width/height:.2f}}}',end='')
